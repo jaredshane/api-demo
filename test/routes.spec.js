566e6559a2fa6139e9fc28e210c5b199f764be5e
Jared Fuller <jaredshane@gmail.com>
2017-04-18 11:35:04 -0500

    added delete test

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -50,4 +50,44 @@ describe('Shows routes', () => {
     })
   })
 
+  describe('POST /api/v1/shows/new', () => {
+    it('should add a new show obj to the db', () => {
+      return chai.request(server)
+      .post('/api/v1/shows/new')
+      .send({
+        name: 'The Simpsons',
+        channel: 'Fox',
+        genre: 'animation',
+        inProduction: true
+      })
+      .then( (res) => {
+        res.should.have.status(201)
+        res.should.be.json
+        res.should.be.a('object')
+      })
+    })
+  })
+
+  describe('DELETE /api/v1/:id', () => {
+    it('should remove a single item from shows', () => {
+      return chai.request(server)
+      .delete('/api/v1/shows/1')
+      .then( (res) => {
+        res.should.have.status(200)
+        res.should.be.json
+        res.body.should.be.a('object')
+
+        chai.request(server)
+        .get('/api/v1/shows')
+        .then( (res) => {
+          res.should.have.status(200)
+          res.should.be.json
+          res.body.should.be.a('array')
+          res.body.length.should.equal(4)
+          res.body[0].should.have.property('name')
+        })
+      })
+    })
+  })
+
 })
