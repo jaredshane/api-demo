3b6869f26c6825421e14b3b2bd5bf172d1580ef7
Jared Fuller <jaredshane@gmail.com>
2017-04-20 10:52:06 -0500

    moved the index.js content to shows.js and required in to index

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,15 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getShows, getShow, getShowFaves, addShow, deleteShow } = require('../controllers/showCtrl');
+router.use(require('./shows'))
 
-router.get('/shows', getShows);
-router.post('/shows/new', addShow)
-router.get('/shows/favorites', getShowFaves);
-router.get('/shows/:id', getShow);
-router.delete('/shows/:id', deleteShow)
+router.get('/', function (req, res) {
+  res.json({
+    "title": "The TV Faves API",
+    "shows": "https://api-demo-jf.herokuapp.com/api/v1/shows",
+    "favorites": "https://api-demo-jf.herokuapp.com/api/v1/shows/favorites?showId=<show_id>",
+    "directors": "https://api-demo-jf.herokuapp.com/api/v1/shows/directors?showId=<show_id>"
+  })
+})
 
 module.exports = router;
